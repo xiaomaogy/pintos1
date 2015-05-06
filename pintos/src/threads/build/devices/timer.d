e31501d49f498069a00c33d0b9239d3d48a7850d
Zimo Li <zimoli@veteran.cs.uchicago.edu>
2015-05-06 18:41:40 -0500

    first commit

---
+++ pintos/src/threads/build/devices/timer.d
@@ -0,0 +1,9 @@
+devices/timer.o: ../../devices/timer.c ../../devices/timer.h \
+ ../../lib/round.h ../../lib/stdint.h ../../lib/debug.h \
+ ../../lib/inttypes.h ../../lib/stdio.h ../../lib/stdarg.h \
+ ../../lib/stdbool.h ../../lib/stddef.h ../../lib/kernel/stdio.h \
+ ../../devices/pit.h ../../threads/interrupt.h ../../threads/synch.h \
+ ../../lib/kernel/list.h ../../threads/thread.h ../../threads/thread.c \
+ ../../lib/random.h ../../lib/string.h ../../threads/flags.h \
+ ../../threads/intr-stubs.h ../../threads/palloc.h ../../threads/switch.h \
+ ../../threads/vaddr.h ../../threads/loader.h
